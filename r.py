72950940866112fff1e0842599dac967b7bde8ef
sorsa0917 <163217992+sorsa0917@users.noreply.github.com>
2024-03-27 11:38:05 +0900

    Add files via upload

--- r.py
+++ r.py
@@ -1,37 +1,30 @@
-import random
-f=open("voca.txt", "r", encoding='UTF-8')
-raw_data=f.read()
-f.close()
-print(raw_data.split("\n")[-1])
-data_list=raw_data.split("\n")
-data_list=data_list[:-1]
-while True:
-    r_index=random.randrange(0,len(data_list))
-    word=data_list[r_index].replace(u"\xa0",u" ").split(" ")[1]
-    if len(word) <= 6 :break
-word =word.upper()
-word_show="_"*len(word)
-print(word_show)
-try_num=0
-ok_list=[]
-no_list=[]
-while True:
-    ans=input().upper()
-    print(ans)
-    result=word.find(ans)
-    print(result)
-    if result == -1:
-        print("오답")
-        try_num+=1
-        no_list.append(ans)
-    else:
-        print("정답")
-        ok_list.append(ans)
-        for i in range(len(word)):
-            if word[i]==ans:
-                word_show=word_show[:i] + ans + word_show[i+1]
-        print(word_show)
-    if try_num==7:break
-    if word_show.find("_")==-1:break
-print(word)
-        
+import pygame
+#1. 게임 초기화
+pygame.init()
+#2. 게임창 옵션 설정
+size=[500, 900]
+screen=pygame.display.set_mode(size)
+title="HANGMAN"
+pygame.display.set_caption(title)
+#3. 게임 내 필요한 설정
+clock=pygame.time.Clock()
+black=(0,0,0)
+white=(255,255,255)
+
+exit=False
+
+#4. 메인 이벤트
+while not exit:
+#.4-1 FPS 설정
+    clock.tick(60)
+#4-2. 각종 입력 감지
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            exit=True
+#4-3. 입력, 시간에 따른 변화
+#4-4. 그리기
+    screen.fill(white)
+#4-5. 업데이트
+    pygame.display.flip()
+#5. 게임종료
+pygame.quit()
